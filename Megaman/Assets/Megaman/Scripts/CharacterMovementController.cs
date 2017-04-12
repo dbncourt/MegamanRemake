be6e30cff468fbfd2035d124b17faae78cac9069
Daniel Betancourt <dbncourt@gmail.com>
2017-04-11 22:12:37 -0600

    Added physics to the player. Added Input controller to handle event via delegates.

---
+++ Megaman/Assets/Megaman/Scripts/CharacterMovementController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using PlayerController.InputController;
+using System.Collections;
+
+public class CharacterMovementController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        InputPlayerController playerController = GetComponent<InputPlayerController>();
+        if (playerController != null)
+        {
+            playerController.BindAxis(AxisInputConstants.HORIZONTAL, Run);
+        }
+        else
+        {
+            Debug.LogError("InputPlayerController not found");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Run(float value)
+    {
+        value *= Time.deltaTime;
+        transform.Translate(value, 0.0f, 0.0f);
+    }
+}
