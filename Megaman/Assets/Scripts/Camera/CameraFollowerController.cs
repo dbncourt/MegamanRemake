688b303f4c06a8bbbb7a567608658f029bfe34bf
Daniel Betancourt <dbncourt@gmail.com>
2017-04-15 17:46:08 -0600

    Set camera to follow horizontally.

---
+++ Megaman/Assets/Scripts/Camera/CameraFollowerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Project.Constants;
+using UnityEngine;
+
+public class CameraFollowerController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform characterToFollow;
+
+    void Start()
+    {
+        if(characterToFollow == null)
+        {
+            Debug.LogError("Character To Follow is not Set");
+        }
+    }
+
+    void Update()
+    {
+        Vector3 position = new Vector3(characterToFollow.position.x, transform.position.y, transform.position.z);
+        transform.SetPositionAndRotation(position, transform.rotation);
+    }
+}
---
+++ Megaman/Assets/Scripts/ProjectConstants/SceneTags.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project.Constants
+{
+    class SceneTags
+    {
+        public static readonly string RESPAWN = "Respawn";
+        public static readonly string FINISH = "Finish";
+        public static readonly string EDITOR_ONLY = "EditorOnly";
+        public static readonly string MAIN_CAMERA = "MainCamera";
+        public static readonly string PLAYER = "Player";
+        public static readonly string GAME_CONTROLLER = "GameController";
+    }
+}
